7741f7d5c7b35ef75b8ff6d80c3447714f1c6315
Emma Mattsson <emma.mattsson02@gmail.com>
2023-04-17 13:04:21 +0200

    x

--- L2/js/script.js
+++ L2/js/script.js
@@ -16,9 +16,34 @@ window.onload = init; // Se till att init aktiveras då sidan är inladdad
 function showFruit(){
     let nr = getInput(inp1Elem, 5);  //Anropar funktionen getInput och refererar till första textfält + värdet 5
     if (nr == -1) return;  
-    let url;  //Deklarerar url som får nytt värde i switch-satsen
     
-    //Kontrollerar nr, fruktbild visas beroende på nummer/case
+    document.getElementById("fruitImg").src = getUrl(nr);
+}
+
+//Kontroll att det skrivs en siffra i första textfältet
+function getInput(elem, high){
+    msgElem.innerHTML = ""
+    let nr = Number(elem.value); //får ett numeriskst värde
+    
+    if (nr < 1 || nr > high){
+        msgElem.innerHTML = "Du måste skriva ett tal mellan 1 och " + high;
+        return -1;
+    }
+    
+    if (isNaN(nr)){
+        msgElem.innerHTML = "Du måste skriva ett tal med siffor";
+        return -1;  
+    }
+    
+    nr = parseInt(nr); //Avrundar till heltal
+    elem.value = nr;
+    
+    return nr;
+} //Slut getInput
+
+function getUrl(nr){
+    let url;  //Url för bilden 
+
     switch (nr){
         case 1: url = "img/apple.png"; break;
         case 2: url = "img/banana.png"; break;
@@ -28,21 +53,7 @@ function showFruit(){
         
         default: url = "img/nofruit.png"; break;
     }
-    
-    //Hämtar referens till elementet med ID "fruitImg"
-    var fruitImg = document.getElementById("fruitImg");
-    fruitImg.src = url;  //Byter ut bilden i img-taggen med annan bild-url
-}
-
-//Kontroll att det skrivs en siffra i första textfältet
-function getInput(elem, high){
-    let nr = Number(elem.value);
-
-    if (isNaN(nr)){
-        msgElem.innerHTML = "Du måste skriva ett tal med siffor";
-        return -1;  
-    }
-    return nr;
-}
+    return url;
+} //Slut getUrl
 
 
