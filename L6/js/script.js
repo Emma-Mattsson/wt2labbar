cfafc85fa0bb98ec0036396d12976dde054e92a9
Emma Mattsson <emma.mattsson02@gmail.com>
2023-05-09 14:47:05 +0200

    x

---
+++ L6/js/script.js
@@ -0,0 +1,16 @@
+// Globala konstanter och variabler
+const roomPrice = [600, 800, 950];		// Pris för rumstyperna
+const facilityPrice = [40, 80, 100];	// Pris för tilläggen
+var formElem;		// Referens till elementet med hela formuläret
+// --------------------------------------------------
+// Initiera globala variabler och händelsehanterare. Lägg till info om pris.
+function init() {
+	formElem = document.getElementById("booking");
+	
+	// Händelsehanterare för textfält som ska kontrolleras
+	
+	// Händelsehanterare för kampanjkod
+	
+} // Slut init
+window.addEventListener("load", init);
+// --------------------------------------------------
